786904c4a33b4eba02a74439c3f209492a3f4ce8
Khan7872463 <155216440+Khan7872463@users.noreply.github.com>
2024-01-02 13:37:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,8 +12,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '20389440'))
-API_HASH = environ.get('API_HASH', 'a1a06a18eb9153e9dbd447cfd5da2457') 
+API_ID = int(environ.get('API_ID', '22853277'))
+API_HASH = environ.get('API_HASH', '5de720af935eae5a2648f3488401d977') 
 BOT_TOKEN = environ.get('BOT_TOKEN', "6514874958:AAFJ1JxyQpceenDmCg418zetEv9nBwiZKoE) 
 
 # Bot settings
@@ -30,7 +30,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ytmak8907:lw4mx9JWSOoBQuIe@cluster0.ljftss6.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
